1aeb339f479f194b55e3c2533dd83abec5f23d3e
dulitharuvin <dulitha.ruvin@gmail.com>
2022-10-11 12:45:14 +0200

    added unit test for repo class

--- BackEnd/BackEnd.Tests/Systems/Repos/TestMontyHallProblemRepo.cs
+++ BackEnd/BackEnd.Tests/Systems/Repos/TestMontyHallProblemRepo.cs
@@ -1,9 +1,27 @@
+using BackEnd.Api.Dtos;
+using BackEnd.Api.Repos;
+using BackEnd.Tests.MockData;
+using FluentAssertions;
+using Xunit;
+
 namespace ackEnd.Tests.Systems.Repos
 {
 
     public class TestMontyHallProblemRep
     {
 
-        
+        [Fact]
+        public async Task GetSimulationResults_ShouldReturnTypeSimulationsResult()
+        {
+            ///Arrange
+            var userInput = MontyHallProblemMockData.GetUserInput();
+            var sut = new MontyHallProblemRepo();
+
+            ///Act
+            var result = await sut.GetSimulationResults(userInput);
+
+            // /// Assert
+            result.GetType().Should().Be(typeof(SimulationsResult));
+        }
     }
 }
